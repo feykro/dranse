37873f9a224f6a0e81c0cf02734a5a20c54cd8ff
feykro <feykrotv@gmail.com>
2021-09-15 16:33:58 +0200

    squelette terminée

--- src/main/webapp/app/dranse/panier/panier.component.ts
+++ src/main/webapp/app/dranse/panier/panier.component.ts
@@ -13,12 +13,30 @@ export class PanierComponent implements OnInit {
   panierId!: number;
   commande!: Commande;
 
+  itemListTest: testCommande[] = [];
+  urlTest =
+    'https://kbimages1-a.akamaihd.net/93affabc-5161-421e-80d5-4477a07b8cee/353/569/90/False/harry-potter-and-the-philosopher-s-stone-3.jpg';
+
+  liste = ['bonjour', 'salut', 'hey'];
+
   constructor(private panierService: PanierService) {
     this.panierId = this.panierService.getPanierId();
     this.commande = this.panierService.getCommande();
+
+    const itemTest = new testCommande(this.urlTest, 'Harry Potter', 'Nicolas Sarkozy', '36.99', 1);
+
+    for (let i = 0; i < 4; i++) {
+      this.itemListTest.push(itemTest);
+    }
   }
 
   ngOnInit(): void {
     console.log('');
+  }
+}
+
+class testCommande {
+  constructor(public urlImage: string, public titre: string, public auteur: string, public prix: string, public qte: number) {
+    const a = 0;
   }
 }
