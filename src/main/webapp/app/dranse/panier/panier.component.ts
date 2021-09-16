7fdd9bd2595a183ca2d43c13c9e9d374382c30a5
feykro <feykrotv@gmail.com>
2021-09-16 12:33:03 +0200

    panier affichage et fonction ok

--- src/main/webapp/app/dranse/panier/panier.component.ts
+++ src/main/webapp/app/dranse/panier/panier.component.ts
@@ -1,5 +1,5 @@
 import { ILivre, Livre } from './../../entities/livre/livre.model';
-import { ILigneCommande, LigneCommande } from './../../entities/ligne-commande/ligne-commande.model';
+import { ILigneCommande } from 'app/entities/ligne-commande/ligne-commande.model';
 import { Component, OnInit } from '@angular/core';
 import { PanierService } from './panier.service';
 import { Commande } from 'app/entities/commande/commande.model';
@@ -12,30 +12,76 @@ import { Commande } from 'app/entities/commande/commande.model';
 export class PanierComponent implements OnInit {
   panierId!: number;
   commande!: Commande;
+  prixTotal = 0;
 
-  itemListTest: testCommande[] = [];
   urlTest =
     'https://kbimages1-a.akamaihd.net/93affabc-5161-421e-80d5-4477a07b8cee/353/569/90/False/harry-potter-and-the-philosopher-s-stone-3.jpg';
 
-  liste = ['bonjour', 'salut', 'hey'];
+  itemListCommande: itemCommande[] = [];
 
   constructor(private panierService: PanierService) {
     this.panierId = this.panierService.getPanierId();
     this.commande = this.panierService.getCommande();
 
-    const itemTest = new testCommande(this.urlTest, 'Harry Potter', 'Nicolas Sarkozy', '36.99', 1);
-
-    for (let i = 0; i < 4; i++) {
-      this.itemListTest.push(itemTest);
-    }
+    this.fakeCommandeInit();
   }
 
   ngOnInit(): void {
     console.log('');
+    this.commande = this.panierService.getCommande();
+  }
+
+  /**
+   * Créer une commande de placeholder
+   */
+  fakeCommandeInit(): void {
+    const itemTest = new itemCommande(this.urlTest, 'Harry Potter', 'Nicolas Sarkozy', '36.99', 1);
+
+    for (let i = 0; i < 4; i++) {
+      this.itemListCommande.push(itemTest);
+      const x: number = +itemTest.prix;
+      this.prixTotal += x * itemTest.qte;
+    }
+  }
+
+  /**
+   * Converti un objet commande en liste d'itemCommande qu'on affiche
+   */
+  convertCommande(): void {
+    const lc = this.commande.ligneCommandes;
+    if (lc === null || lc === undefined) {
+      return;
+    }
+    for (const ligne of lc) {
+      this.itemListCommande.push(this.convertLigneCommande(ligne));
+    }
+  }
+
+  convertLigneCommande(ligne: ILigneCommande): itemCommande {
+    let bookTitle = '';
+    let bookAuthor = '';
+    let bookPrice = '';
+    let quantite = 0;
+
+    const book = ligne.livre;
+
+    book?.titre === undefined || book.titre === null ? (bookTitle = 'undefinedTitled') : (bookTitle = book.titre);
+    book?.auteur === undefined || book.auteur === null ? (bookAuthor = 'underfinedAuthor') : (bookAuthor = book.auteur);
+    ligne.quantite === undefined || ligne.quantite === null ? (quantite = -1) : (quantite = ligne.quantite);
+
+    if (!(book?.prix === undefined || book.prix === null)) {
+      this.prixTotal += book.prix;
+      bookPrice = book.prix.toString();
+    }
+
+    return new itemCommande(this.urlTest, bookTitle, bookAuthor, bookPrice, quantite);
   }
 }
 
-class testCommande {
+/**
+ * Cette classe permet de peupler une liste et d'afficher les objets contenus par la commande
+ */
+class itemCommande {
   constructor(public urlImage: string, public titre: string, public auteur: string, public prix: string, public qte: number) {
     const a = 0;
   }
