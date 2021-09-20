c7de4e19872272e786cb34283c2b7f116ce26500
feykro <feykrotv@gmail.com>
2021-09-20 16:26:37 +0200

    commit before merge

---
+++ src/main/webapp/app/dranse/service/categories-controller-ressource.service.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs';
+import { ICategorie } from './../../entities/categorie/categorie.model';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApplicationConfigService } from 'app/core/config/application-config.service';
+
+export type EntityResponseType = HttpResponse<ICategorie>;
+export type EntityArrayResponseType = HttpResponse<ICategorie[]>;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesControllerRessourceService {
+  protected resourceUrl = this.applicationConfigService.getEndpointFor('/api/v1/categorie');
+
+  constructor(private http: HttpClient, protected applicationConfigService: ApplicationConfigService) {}
+
+  getPopularCategories(): Observable<EntityArrayResponseType> {
+    return this.http.get<ICategorie[]>(`${this.resourceUrl}/get-most-popular`, { observe: 'response' });
+  }
+}
