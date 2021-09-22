7d385425c3537882fd104aad344f27bd313c38d0
Jenna <Shinoda.jesse@gmail.com>
2021-09-22 16:28:47 +0200

    using elasticsearch for searching by title

--- src/main/java/fr/dranse/myapp/repository/search/LivreSearchRepository.java
+++ src/main/java/fr/dranse/myapp/repository/search/LivreSearchRepository.java
@@ -1,9 +1,16 @@
 package fr.dranse.myapp.repository.search;
 
 import fr.dranse.myapp.domain.Livre;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 /**
  * Spring Data Elasticsearch repository for the {@link Livre} entity.
  */
-public interface LivreSearchRepository extends ElasticsearchRepository<Livre, Long> {}
+public interface LivreSearchRepository extends ElasticsearchRepository<Livre, Long> {
+
+    @Query("{\"fuzzy\" : {\"titre\" : {\"value\" : \"?0\", \"fuzziness\": \"AUTO\"}}}")
+    Page<Livre> searchByTitle(String titre, Pageable pageable);
+}
