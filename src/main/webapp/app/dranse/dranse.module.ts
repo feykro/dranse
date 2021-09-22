727fb663ed83fb96f86a5905dbd66653ef99ddbb
Hugo <hugoland37@gmail.com>
2021-09-22 13:41:28 +0200

    small changes to historique : displays individual price of books (independent of quantity) and displays order's total price

--- src/main/webapp/app/dranse/dranse.module.ts
+++ src/main/webapp/app/dranse/dranse.module.ts
@@ -7,9 +7,19 @@ import { PanierComponent } from './panier/panier.component';
 import { FormspaiementComponent } from './formspaiement/formspaiement.component';
 import { FormsModule } from '@angular/forms';
 import { ConfirmationachatComponent } from './confirmationachat/confirmationachat.component';
+import { HistoriqueComponent } from './historique/historique.component';
+import { PageRechercheComponent} from './page-recherche/page-recherche.component';
+
 
 @NgModule({
-  declarations: [ProduitComponent, AccueilComponent, PanierComponent, FormspaiementComponent, HistoriqueComponent, ConfirmationachatComponent],
+  declarations: [
+  ProduitComponent,
+   AccueilComponent,
+   PanierComponent,
+   FormspaiementComponent,
+   HistoriqueComponent,
+   ConfirmationachatComponent,
+   PageRechercheComponent],
   imports: [CommonModule, DranseRoutingModule, FormsModule],
 })
 export class DranseModule {}
--- src/main/webapp/app/dranse/historique/historique.component.ts
+++ src/main/webapp/app/dranse/historique/historique.component.ts
@@ -4,6 +4,8 @@ import { CommandeControllerRessourceService } from '../service/commande-controll
 import { HistoriqueService } from './historique.service';
 import { ICommande } from 'app/entities/commande/commande.model';
 import * as dayjs from 'dayjs';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-historique',
@@ -35,6 +37,31 @@ export class HistoriqueComponent implements OnInit  {
 
   displayDate(date: dayjs.Dayjs): string {
     return dayjs(date).toString();
+  }
+
+  significantDigits(price : number): string {
+    return price.toFixed(2);
+  }
+
+  sumCommand(command : ICommande) : number {
+
+    let sum = 0;
+    for (const item of command.ligneCommandes!){
+      sum += item.prixPaye!;
+    }
+    return sum;
+
+    /*
+    const commandeRequest: Observable<HttpResponse<ICommande>> = <Observable<HttpResponse<ICommande>>>(
+          this.commandService.getCommande(id)
+        );
+        commandeRequest.subscribe(value => {
+          var sum = 0;
+          for (let item of <ILigneCommande[]>value.body.ligneCommandes){
+            sum += item.prixPaye!;
+          }
+        });
+        return sum;*/
   }
 
 
